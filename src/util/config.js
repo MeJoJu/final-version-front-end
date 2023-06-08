fc997817f9dd52488aec4982f1d883164f7f5524
junzhou <jun@gmail.com>
2023-06-08 11:39:07 +1000

    first commit

---
+++ src/util/config.js
@@ -0,0 +1,6 @@
+
+export let api = 'http://192.168.1.105';
+export const config = {
+  //BASE_URL: `${api}:5000/api`, 
+  BASE_URL: 'https://beansceneapplication01.herokuapp.com/api' // Jun Zhou's app server
+};
